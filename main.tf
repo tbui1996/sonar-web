ed019a8937cb64970092f58f3b405f2e2358692c
Tony Duco <nosduco@gmail.com>
2021-04-27 12:20:32 -0400

    add terraform backend

--- main.tf
+++ main.tf
@@ -1,4 +1,13 @@
 terraform {
+  backend "remote" {
+    hostname = "app.terraform.io"
+    organization = "circulo"
+
+    workspaces {
+      name = "sonar-web"
+    }
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
