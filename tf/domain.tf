299258872dcb65122ea5ea8da7c64ae8b770aee0
Tony Duco <nosduco@gmail.com>
2021-08-06 15:38:01 -0400

    [NOSTORY]: Add DNS records for ACM cert validation

--- tf/domain.tf
+++ tf/domain.tf
@@ -17,6 +17,23 @@ resource "aws_acm_certificate_validation" "validate_web" {
   validation_record_fqdns = [aws_route53_record.web_route53.fqdn]
 }
 
+resource "aws_route53_record" "web_route53_dns" {
+  for_each = {
+    for dvo in aws_acm_certificate.web.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+  name            = each.value.name
+  records         = [each.value.record]
+  type            = each.value.type
+  zone_id         = var.zone_id
+  ttl             = 60
+  allow_overwrite = true
+}
+
+
 resource "aws_route53_record" "web_route53" {
   zone_id = var.zone_id
   name    = var.domain_name
